9ed5067291169e3ff9c75e59ab4b5503e20c5174
MS269 <ehddnr9797@gmail.com>
2022-02-08 19:11:48 +0900

    Commit 220208

---
+++ practice/search/연결_요소의_개수.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+for i in range(m):
+    u, v = map(int, input().split())
+
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+def dfs(v):
+    visited[v] = True
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+
+ans = 0
+
+for i in range(1, n + 1):
+    if not visited[i]:
+        dfs(i)
+        ans += 1
+
+print(ans)
